5f8a431aa0c9b3eaa4c9db369a5052aae22b9800
firmankhoiril <firmankhoiril13@gmail.com>
2023-05-25 16:49:13 +0700

    change alt

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { Footer, Navbar } from "@/components";
 import "./globals.css";
 import { QueryClientProvider, QueryClient } from "react-query";
--- components/CardImage.tsx
+++ components/CardImage.tsx
@@ -8,7 +8,7 @@ import React from "react";
 const CardImage = ({ item }: ICardImage) => {
   return (
     <Link href={`/${item?.video?.videoId}`} className="card cursor-pointer hover:scale-105 transition duration-150 w-80 bg-white shadow-xl">
-      <Image src={item?.video?.thumbnails[0].url} width={320} height={140} alt="Shoes" />
+      <Image src={item?.video?.thumbnails[0].url} width={320} height={140} alt={item?.video?.author?.title} />
       <div className="card-body">
         <h4 className="card-title text-sm font-inter">{item?.video?.title}</h4>
         <p className="text-[12px]">{item?.video?.descriptionSnippet}</p>
