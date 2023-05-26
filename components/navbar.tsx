3e3a4478a1d92527e2a4e0cdc0a87406d98d3a54
firmankhoiril <firmankhoiril13@gmail.com>
2023-05-26 13:49:03 +0700

    change bg and text

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -5,21 +5,21 @@ import SearchBar from "./SearchBar";
 
 const navbar = () => {
   return (
-    <div className="px-4 py-2 flex justify-between bg-white sticky top-0 z-10 items-center flex-wrap">
+    <div className="px-4 py-2 flex justify-between bg-pink-500 sticky top-0 z-10 items-center flex-wrap">
       <div className="box__center">
         <div data-tip="Menu" className=" icon__left ">
           <AiOutlineMenu className=" icon" />
         </div>
         <Link href="/">
           <h2 className="font-extrabold text-3xl text-black tracking-wide">
-            My<span className="text-red-400">Tube</span>
+            Pink<span className="text-white">Tube</span>
           </h2>
         </Link>
       </div>
 
       <div className="box__center">
         <div data-tip="Search" className="icon__right block sm:hidden">
-          <AiOutlineSearch className=" icon" />
+          <AiOutlineSearch className="icon" />
         </div>
         <SearchBar />
       </div>
