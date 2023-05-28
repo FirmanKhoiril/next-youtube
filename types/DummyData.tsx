be1b6702aec2b0b63aa436e2889ee591c5ccbe74
firmankhoiril <firmankhoiril13@gmail.com>
2023-05-28 16:57:47 +0700

    test fix

--- types/DummyData.tsx
+++ types/DummyData.tsx
@@ -1,6 +1,6 @@
 import { AiOutlineHome } from "react-icons/ai";
 import { TfiShortcode } from "react-icons/tfi";
-import { TDummyLinks } from "./Types";
+
 export const dummyLinks = [
   { name: "Home", to: "/", icon: <AiOutlineHome /> },
   { name: "Shorts", to: "/short", icon: <TfiShortcode /> },
